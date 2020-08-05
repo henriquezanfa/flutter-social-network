9110be173bb9060086ebec199337253f3d90ee36
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-05 00:21:54 -0300

    Fetching data from firestore

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/header.dart';
+import '../widgets/progress.dart';
+
+final userRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,10 +13,30 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text('TimeLine'),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: userRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Text> children = snapshot.data.documents
+              .map((doc) => Text(doc['username']))
+              .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 }
