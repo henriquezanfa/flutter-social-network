506576d022b5361f289645d560ab3522ef976f7c
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-04 23:47:16 -0300

    Progress indicators

--- lib/widgets/progress.dart
+++ lib/widgets/progress.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 circularProgress() {
-  return Text("circular progress");
+  return Container(
+    alignment: Alignment.center,
+    padding: EdgeInsets.only(top: 8.0),
+    child: CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.purple),
+    ),
+  );
 }
 
 linearProgress() {
-  return Text("linear progress");
+  return Container(
+    padding: EdgeInsets.only(bottom: 8.0),
+    child: LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Colors.purple),
+    ),
+  );
 }
