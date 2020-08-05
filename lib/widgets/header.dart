ce0f3ccbdb2e870c8b3800db453685eac41c9177
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-04 23:40:37 -0300

    Common header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'FlutterShare' : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? 'Signatra' : '',
+        fontSize: isAppTitle ? 50.0 : 22,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
